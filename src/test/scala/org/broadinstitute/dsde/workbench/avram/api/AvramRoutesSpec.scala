78776283c3b6b29a5fd0c335aba28bf0ee3b016e
ansingh7115 <singh.an@husky.neu.edu>
2018-08-07 15:28:52 -0400

    does this work

--- src/test/scala/org/broadinstitute/dsde/workbench/avram/api/AvramRoutesSpec.scala
+++ src/test/scala/org/broadinstitute/dsde/workbench/avram/api/AvramRoutesSpec.scala
@@ -4,6 +4,7 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class AvramRoutesSpec extends FlatSpec with Matchers {
 
+  //some comments and stuff
   "AvramRoutes" should "should return Pong() on ping" in {
     val avramRoutes = new AvramRoutes()
     avramRoutes.ping shouldEqual Pong()
