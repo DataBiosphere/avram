f415d4239c4e4bbdcbdadc9da0b12c91ce2e6681
ansingh7115 <singh.an@husky.neu.edu>
2018-08-15 17:07:40 -0400

    where is this techops crap coming from?

--- deploy.sh
+++ deploy.sh
@@ -19,27 +19,29 @@ GIT_BRANCH=$2
 #	#exit 1
 #fi
 
-ENVIRONMENT="dev"
-
-GOOGLE_PROJECT=broad-avram-$ENVIRONMENT
-
-# pull the credentials for the service account
-docker run --rm -e VAULT_TOKEN=$VAULT_TOKEN broadinstitute/dsde-toolbox vault read --format=json "secret/dsde/avram/$ENVIRONMENT/deploy-account.json" | jq .data > deploy_account.json
-
-gcloud auth activate-service-account --key-file=deploy_account.json
-
-# get appenginge sdk
-curl 'https://storage.googleapis.com/appengine-sdks/featured/appengine-java-sdk-1.9.64.zip' > /tmp/appengine.zip
-unzip /tmp/appengine.zip
-export APPENGINE_SDK_HOME=$PWD/appengine-java-sdk-1.9.64
-
-# deploy endpoints
-gcloud endpoints services deploy $PWD/openapi.yaml
-
-# deploy backend
-sbt package
-$PWD/appengine-java-sdk-1.9.64/bin/appcfg.sh --service_account_json_key_file=deploy_account.json update $PWD/target/webapp
-
+printenv
+#
+#ENVIRONMENT="dev"
+#
+#GOOGLE_PROJECT=broad-avram-$ENVIRONMENT
+#
+## pull the credentials for the service account
+#docker run --rm -e VAULT_TOKEN=$VAULT_TOKEN broadinstitute/dsde-toolbox vault read --format=json "secret/dsde/avram/$ENVIRONMENT/deploy-account.json" | jq .data > deploy_account.json
+#
+#gcloud auth activate-service-account --key-file=deploy_account.json
+#
+## get appenginge sdk
+#curl 'https://storage.googleapis.com/appengine-sdks/featured/appengine-java-sdk-1.9.64.zip' > /tmp/appengine.zip
+#unzip /tmp/appengine.zip
+#export APPENGINE_SDK_HOME=$PWD/appengine-java-sdk-1.9.64
+#
+## deploy endpoints
+#gcloud endpoints services deploy $PWD/openapi.yaml
+#
+## deploy backend
+#sbt package
+#$PWD/appengine-java-sdk-1.9.64/bin/appcfg.sh --service_account_json_key_file=deploy_account.json update $PWD/target/webapp
+#
 
 ##render the endpoints json and then deploy it
 #docker run -v $PWD/startup.sh:/app/startup.sh \
