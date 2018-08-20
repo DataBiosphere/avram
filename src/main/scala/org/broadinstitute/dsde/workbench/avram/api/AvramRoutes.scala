a3749ab5d95e8798299615264b41627735e92146
ansingh7115 <singh.an@husky.neu.edu>
2018-08-20 18:14:30 -0400

    add the correct labels for the resource

--- src/main/scala/org/broadinstitute/dsde/workbench/avram/api/AvramRoutes.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/avram/api/AvramRoutes.scala
@@ -7,7 +7,7 @@ import org.broadinstitute.dsde.workbench.avram.util.Logger
 case class Pong()
 
 
-@Api(name = "avram", version = "v1", scopes = Array("https://www.googleapis.com/auth/userinfo.email"), audiences = Array(""))
+@Api(name = "avram", version = "v1", scopes = Array("https://www.googleapis.com/auth/userinfo.email"))
 class AvramRoutes {
 
   private val log = new Logger
--- src/main/scala/org/broadinstitute/dsde/workbench/avram/util/Logger.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/avram/util/Logger.scala
@@ -1,26 +1,35 @@
 package org.broadinstitute.dsde.workbench.avram.util
 
 import java.util.Collections
-
+import java.util.Map
 import com.google.cloud.MonitoredResource
 import com.google.cloud.logging.Payload.StringPayload
 import com.google.cloud.logging.{LogEntry, Logging, LoggingOptions, Severity}
+import scala.collection.JavaConverters._
 
+/*
+ Logger allows logging to Stackdriver Logging
+ */
 class Logger {
 
   private val logging: Logging = LoggingOptions.getDefaultInstance.getService
 
-  private val logName: String = "avram"
+  // this could be passed to the Logger as a parameter
+  private val logName = "avram"
+
+  //these values should come from config
+  private val projectId = "broad-avram-dev"
+
+  private val resourceType = "cloud_debugger_resource"
 
   private def entry(text: String): LogEntry = LogEntry
     .newBuilder(StringPayload.of(text))
     .setSeverity(Severity.INFO)
     .setLogName(logName)
-    .setResource(MonitoredResource.newBuilder("cloud_debugger_resource").build)
+    .setResource(MonitoredResource.newBuilder(resourceType).setLabels(Map("project_id" -> projectId, "app" -> "avram").asJava).build)
     .build
 
   def log(message: String) = {
     logging.write(Collections.singleton(entry(message)))
   }
-
 }
