1df028cd9669ba96bdfc2e12813a0ee79913c50e
ansingh7115 <singh.an@husky.neu.edu>
2018-08-20 16:36:58 -0400

    remove configs for now, add that in a later pr

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
-# avram
+# Avram
 
-Entity Service
+Avram is a metadata manager that allows analysts to create and manage [meta]data entries that follow 
+flexible, user-defined models. 
 
 
+## Building and Running
+
+Clone the repo.
+```
+$ git clone https://github.com/DataBiosphere/avram.git 
+$ cd avram
+```
+
+/// Add instructions for pulling avram docker image and running that ///
+
+
+### Run unit tests
+Build and run tests.
+```
+sbt clean compile test
+```
 
--- src/main/scala/org/broadinstitute/dsde/workbench/avram/api/AvramRoutes.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/avram/api/AvramRoutes.scala
@@ -1,13 +1,12 @@
 package org.broadinstitute.dsde.workbench.avram.api
 
 import com.google.api.server.spi.config.{Api, ApiMethod}
-import org.broadinstitute.dsde.workbench.avram.util.Logger
 
 case class Pong()
 
-@Api(name = "avram", version = "v1", scopes = Array("https://www.googleapis.com/auth/userinfo.email"), clientIds = Array(""), audiences = Array(""))
+
+@Api(name = "avram", version = "v1", scopes = Array("https://www.googleapis.com/auth/userinfo.email"), audiences = Array(""))
 class AvramRoutes {
-  val logger = new Logger
 
   @ApiMethod(name = "ping", httpMethod = "get", path = "ping")
   def ping: Pong = {
