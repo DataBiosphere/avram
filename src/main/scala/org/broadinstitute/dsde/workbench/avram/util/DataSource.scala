be629f56497c881fa0f72952db6ce1f4d03a6ace
Brian Reilly <breilly@broadinstitute.org>
2018-08-20 11:45:38 -0400

    Added DB connection pool using Apache DBCP

---
+++ src/main/scala/org/broadinstitute/dsde/workbench/avram/util/DataSource.scala
@@ -0,0 +1,19 @@
+package org.broadinstitute.dsde.workbench.avram.util
+
+import com.typesafe.config.{Config, ConfigFactory}
+import org.apache.commons.dbcp2.BasicDataSource
+import slick.jdbc.PostgresProfile.api._
+
+object DataSource {
+  private val config = ConfigFactory.load().getConfig("dbcpDataSource")
+
+  // See https://commons.apache.org/proper/commons-dbcp/configuration.html for configuration options and defaults
+  val ds = new BasicDataSource()
+  ds.setDriverClassName(config.getString("driverClassName"))
+  ds.setUrl(config.getString("url"))
+  ds.setUsername(config.getString("username"))
+  ds.setPassword(config.getString("password"))
+  ds.setMaxTotal(config.getInt("maxTotal"))
+
+  val database = Database.forDataSource(ds, Option(ds.getMaxTotal), AsyncExecutor("Avram Executor", config.getInt("slick.numThreads"), config.getInt("slick.queueSize")))
+}
