c0fbc5c099b921531d9a66078295e7dc80c39c8f
Brian Reilly <breilly@broadinstitute.org>
2018-09-25 08:10:32 -0400

    Small refactor of common test data.

--- src/test/scala/org/broadinstitute/dsde/workbench/avram/CommonTestData.scala
+++ src/test/scala/org/broadinstitute/dsde/workbench/avram/CommonTestData.scala
@@ -1,21 +1,32 @@
 package org.broadinstitute.dsde.workbench.avram
 
-import com.typesafe.config.ConfigFactory
 import org.broadinstitute.dsde.workbench.avram.config.DbcpDataSourceConfig
 import org.broadinstitute.dsde.workbench.avram.util.SlickDatabaseFactory
-import net.ceedubs.ficus.Ficus._
-import net.ceedubs.ficus.readers.ArbitraryTypeReader._
 
-// values common to multiple tests, to reduce boilerplate
+/**
+  * Values common to multiple tests, to reduce boilerplate.
+  */
+object CommonTestData {
 
-trait CommonTestData {
+  /**
+    * At the time of writing, this mirrors the values in src/test/resources/app.conf. It may not be
+    * necessary to duplicate them here, but I wanted to illustrate that test configuration can live
+    * in code as long as we control the instance lifecycle of the module being tested (i.e., not
+    * @Api-annotated endpoints which are managed by the Google Cloud Endpoints servlet). The
+    * benefits of config-in-code are less indirection and improved clarity.
+    */
+  val localDataSourceConfig = DbcpDataSourceConfig(
+    driverClassName = "org.postgresql.Driver",
+    url = "jdbc:postgresql://127.0.0.1:5432/testdb",
+    username = "avram",
+    password = "test",
+    maxTotal = 20,
+    slickNumThreads = 10,
+    slickQueueSize = 1000
+  )
 
-  val configFactory = ConfigFactory.parseResources("app.conf").withFallback(ConfigFactory.load())
-  private val dbcpDataSourceConfig = configFactory.as[DbcpDataSourceConfig]("dbcpDataSource")
-  val dataSource = new SlickDatabaseFactory(dbcpDataSourceConfig)
+  val localDatabase = new SlickDatabaseFactory(localDataSourceConfig).database
 
   //val entity1 = ...
 
 }
-
-
