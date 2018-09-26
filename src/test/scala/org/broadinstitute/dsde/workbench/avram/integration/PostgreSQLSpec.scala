4fc4301cdc127ba79753028b79b9554cd71d4a40
Brian Reilly <breilly@broadinstitute.org>
2018-09-26 13:34:34 -0400

    Small refactor of common test data. (#10)

--- src/test/scala/org/broadinstitute/dsde/workbench/avram/integration/PostgreSQLSpec.scala
+++ src/test/scala/org/broadinstitute/dsde/workbench/avram/integration/PostgreSQLSpec.scala
@@ -7,16 +7,15 @@ import slick.jdbc.PostgresProfile.api._
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 
-class PostgreSQLSpec extends FreeSpec with CommonTestData {
+class PostgreSQLSpec extends FreeSpec {
+
+  val database = CommonTestData.localDatabase
 
   "should fetch" - {
     "using slick" in {
-      val db = dataSource.database
-      try {
-        val action: DBIO[Seq[String]] = sql"select now()".as[String]
-        val value: Seq[String] = Await.result(db.run(action), Duration.Inf)
-        value foreach { v => println(s"from slick: $v") }
-      } finally db.close
+      val action: DBIO[Seq[String]] = sql"select now()".as[String]
+      val value: Seq[String] = Await.result(database.run(action), Duration.Inf)
+      value foreach { v => println(s"from slick: $v") }
     }
   }
 }
