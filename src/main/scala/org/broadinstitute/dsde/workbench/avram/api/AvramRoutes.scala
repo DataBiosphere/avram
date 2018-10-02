8e93ba072b27cc5c4a6d449f0a34135db39fe06c
Brian Reilly <breilly@broadinstitute.org>
2018-10-02 09:30:31 -0400

    Added auth endpoint with tests

--- src/main/scala/org/broadinstitute/dsde/workbench/avram/api/AvramRoutes.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/avram/api/AvramRoutes.scala
@@ -3,7 +3,11 @@ package org.broadinstitute.dsde.workbench.avram.api
 import java.util.logging.Logger
 
 import com.google.api.server.spi.config.{Api, ApiMethod}
+import com.google.api.server.spi.response.UnauthorizedException
+import javax.servlet.http.HttpServletRequest
 import org.broadinstitute.dsde.workbench.avram.Avram
+import org.broadinstitute.dsde.workbench.avram.dataaccess.SamUserInfoResponse
+import org.broadinstitute.dsde.workbench.avram.util.ErrorResponse
 import slick.jdbc.PostgresProfile.api._
 
 import scala.beans.BeanProperty
@@ -20,12 +24,28 @@ case class DbPoolStats(@BeanProperty numActive: Int, @BeanProperty numIdle: Int,
 class AvramRoutes {
 
   private val log = Logger.getLogger(getClass.getName)
-
+  private val samDao = Avram.samDao
   private val database = Avram.database
 
   @ApiMethod(name = "ping", httpMethod = "get", path = "ping")
   def ping: Pong = {
     Pong()
+  }
+
+  @ApiMethod(name = "authPing", httpMethod = "get", path = "authPing")
+  def authPing(r: HttpServletRequest): Pong = {
+    getToken(r) match {
+      case None => throw new UnauthorizedException("Missing access token")
+      case Some(token) =>
+        val result: Either[ErrorResponse, SamUserInfoResponse] = samDao.getUserStatus(token)
+        result match {
+          case Left(error) => throw error.exception
+          case Right(userInfo) =>
+            log.info(userInfo.userEmail)
+            log.info(userInfo.userSubjectId)
+            Pong()
+        }
+    }
   }
 
   // TODO: remove this endpoint when we have more meaningful ways to test database queries
@@ -44,6 +64,12 @@ class AvramRoutes {
         sql"select count(*) from pg_stat_activity where pid <> pg_backend_pid() and usename = current_user".as[Int])
     } yield DbPoolStats(dataSource.getNumActive, dataSource.getNumIdle, totalConnections.head)
     Await.result(result, Duration.Inf)
+  }
+
+  private def getToken(req: HttpServletRequest) = {
+    Option(req.getHeader("Authorization")) map {
+      _.stripPrefix("Bearer ")
+    }
   }
 
   private def fetchTimestampFromDBWithSlick(): String = {
---
+++ src/test/scala/org/broadinstitute/dsde/workbench/avram/api/AuthenticationSpec.scala
@@ -0,0 +1,26 @@
+package org.broadinstitute.dsde.workbench.avram.api
+
+import com.google.api.server.spi.response.UnauthorizedException
+import org.broadinstitute.dsde.workbench.avram.dataaccess.MockSam
+import org.scalatest.{FreeSpec, Matchers}
+
+class AuthenticationSpec extends FreeSpec with Matchers with MockSam {
+
+  override def samPort = 9999
+  val api = new AvramRoutes
+
+  "authorized ping" - {
+    "returns 401 when auth token is not given" in {
+      val r = new FakeHttpServletRequest()
+      an [UnauthorizedException] should be thrownBy api.authPing(r)
+    }
+
+    "pongs when auth token is valid" in {
+      val token = "test"
+      mockSam.addValidAuthentication(token, "123", "test@dummy.org")
+
+      val r = new FakeHttpServletRequest(headers = Map("Authorization" -> List(s"Bearer $token")))
+      api.authPing(r) shouldEqual Pong()
+    }
+  }
+}
