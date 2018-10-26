90d8f7e1f51245760075abc18d7357947a6f5078
ansingh7115 <singh.an@husky.neu.edu>
2018-10-26 00:56:07 -0400

    works now

--- src/main/scala/org/broadinstitute/dsde/workbench/avram/api/AvramServlet.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/avram/api/AvramServlet.scala
@@ -1,5 +1,6 @@
 package org.broadinstitute.dsde.workbench.avram.api
 
+import java.util.logging.Logger
 
 import scala.concurrent.duration.Duration
 import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
@@ -7,43 +8,48 @@ import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
 import org.broadinstitute.dsde.workbench.avram.Avram
 import org.broadinstitute.dsde.workbench.avram.dataaccess.SamUserInfoResponse
 import org.broadinstitute.dsde.workbench.avram.model.AvramException
-import org.broadinstitute.dsde.workbench.avram.util.ErrorResponse
 
 import scala.concurrent.Await
-import scala.util.{Failure, Success}
-//import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import scala.util.{Failure, Success}
+
 
 trait AvramServlet {
 
   implicit val executionContext = scala.concurrent.ExecutionContext.Implicits.global
 
+  val log = Logger.getLogger(getClass.getName)
   private val samDao = Avram.samDao
   private val bearerPattern = """(?i)bearer (.*)""".r
 
-  def registerOnComplete[T]
-  (future: Future[T], request: HttpServletRequest, response: HttpServletResponse)
-  (f: T => Unit): Future[T] = {
+  def registerOnComplete[T](future: Future[T], request: HttpServletRequest, response: HttpServletResponse) = {
     future onComplete {
-      case Success(result) => f(result)
+      case Success(result) => {
+        response.setStatus(HttpServletResponse.SC_OK)
+      }
       case Failure(e: AvramException) => {
         response.setStatus(e.status)
         response.getWriter.write(e.message)
       }
-      case Failure(e) => response.getWriter.write(e.getMessage)
+      case Failure(e) => {
+        response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR)
+        response.getWriter.write(e.getMessage)
+      }
     }
     future
   }
 
-
   def handleAuthenticatedRequest[T]
   (request: HttpServletRequest, response: HttpServletResponse)
   (f: SamUserInfoResponse => Future[Either[AvramException, T]]): T = {
-    val thing = for {
-      userInfo <- extractUserInfo(request)
-      result <- f(userInfo)
-    } yield userInfo
-    unsafeRun(Await.result(f(thing), Duration.apply(30, "second")))
+    unsafeRun {
+      for {
+        userInfo <- extractUserInfo(request)
+        result <- Await.result(f(userInfo), Duration.apply(30, "second"))
+      } yield {
+        result
+      }
+    }
   }
 
   private def unsafeRun[T](f: => Either[AvramException, T]): T = {
@@ -54,14 +60,19 @@ trait AvramServlet {
     for {
       token <- extractBearerToken(r)
       userInfo <- samDao.getUserStatus(token)
-    } yield userInfo
+    } yield {
+      userInfo
+    }
+
   }
 
   private def extractBearerToken(r: HttpServletRequest): Either[AvramException, String] = {
     val token = for {
       value <- Option(r.getHeader("Authorization"))
       tokenMatch <- bearerPattern.findPrefixMatchOf(value)
-    } yield tokenMatch.group(1)
+    } yield {
+      tokenMatch.group(1)
+    }
     token.toRight(AvramException(HttpServletResponse.SC_NOT_FOUND, "Auth token not found."))
   }
 }
--- src/main/scala/org/broadinstitute/dsde/workbench/avram/service/AvramService.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/avram/service/AvramService.scala
@@ -6,6 +6,6 @@ import org.broadinstitute.dsde.workbench.avram.Avram
 
 
 trait AvramService {
-  private val log = Logger.getLogger(getClass.getName)
+  val log = Logger.getLogger(getClass.getName)
   val database = Avram.database
 }
