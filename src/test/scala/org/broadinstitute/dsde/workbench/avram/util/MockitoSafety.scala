383d5ae8c72e0ed910f7ba98a90dfa795507bc66
Brian Reilly <breilly@broadinstitute.org>
2018-10-30 12:50:35 -0400

    Updated comment

--- src/test/scala/org/broadinstitute/dsde/workbench/avram/util/MockitoSafety.scala
+++ src/test/scala/org/broadinstitute/dsde/workbench/avram/util/MockitoSafety.scala
@@ -6,7 +6,7 @@ import org.scalatest.mockito.MockitoSugar
 /**
   * Convenience mash-up of MockitoSugar and OneInstancePerTest. Mockito mocks are stateful and
   * therefore must not be shared between tests. Using OneInstancePerTest creates test case isolation
-  * whice allowing mocks to be instance variables, including having default behavior, without the
-  * extra ceremony of fixture factory functions.
+  * whice allowing mocks to be instance variables as opposed to the extra ceremony of fixture
+  * factory functions.
   */
 trait MockitoSafety extends MockitoSugar with OneInstancePerTest { self: Suite => }
