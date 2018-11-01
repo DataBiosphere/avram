b06ee21b1b8170f9ae6748a0e1ecdc76980a2cec
Brian Reilly <breilly@broadinstitute.org>
2018-11-01 11:12:46 -0400

    Found a way to not leave cruft in SamDao

--- src/main/scala/org/broadinstitute/dsde/workbench/avram/dataaccess/HttpSamDao.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/avram/dataaccess/HttpSamDao.scala
@@ -13,37 +13,17 @@ import org.broadinstitute.dsde.workbench.avram.util.{AvramResult, RestClient}
 class HttpSamDao(samUrl: String) extends SamDao with RestClient {
   private val log: Logger = Logger.getLogger(getClass.getName)
 
-  // This function is deprecated and no longer has test coverage. Migrate to getUserStatus after merge of PR #22
-  override def getUserStatus_deprecated(token: String): Either[AvramException, SamUserInfoResponse] = {
-    // Temporarily put sttp backend in implicit scope here instead of RestClient
-    implicit val backend = sttpBackend
-
-    val request = buildAuthenticatedGetRequest(samUrl, "/register/user/v2/self/info", token)
-    for {
-      response <- request.send()
-      content <- response.body                 leftMap errorResponseToAvramException(response.code)
-      json <- parse(content)                   leftMap circeErrorToAvramException
-      userInfo <- json.as[SamUserInfoResponse] leftMap circeErrorToAvramException
-    } yield userInfo
-  }
-
   override def getUserStatus(token: String): AvramResult[SamUserInfoResponse] = {
-    // Temporarily put sttp backend in implicit scope here instead of RestClient
-    implicit val backend = catsSttpBackend
-
     val request = buildAuthenticatedGetRequest(samUrl, "/register/user/v2/self/info", token)
     for {
       response <- AvramResult.fromIO(request.send())
-      content <- AvramResult.fromEither(response.body leftMap responseToErrorResponse(response))
-      json <- AvramResult.fromEither(parse(content) leftMap circeErrorToErrorResponse)
-      userInfo <- AvramResult.fromEither(json.as[SamUserInfoResponse] leftMap circeErrorToErrorResponse)
+      content <- AvramResult.fromEither(response.body leftMap errorResponseToAvramException(response.code))
+      json <- AvramResult.fromEither(parse(content) leftMap circeErrorToAvramException)
+      userInfo <- AvramResult.fromEither(json.as[SamUserInfoResponse] leftMap circeErrorToAvramException)
     } yield userInfo
   }
 
   override def queryAction(samResource: String, action: String, token: String): AvramResult[Boolean] = {
-    // Temporarily put sttp backend in implicit scope here instead of RestClient
-    implicit val backend = catsSttpBackend
-
     val request = buildAuthenticatedGetRequest(samUrl, s"/api/resources/v1/entity-collection/$samResource/action/$action", token)
     for {
       response <- AvramResult.fromIO(request.send())
--- src/main/scala/org/broadinstitute/dsde/workbench/avram/dataaccess/SamDao.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/avram/dataaccess/SamDao.scala
@@ -4,8 +4,6 @@ import org.broadinstitute.dsde.workbench.avram.model.AvramException
 import org.broadinstitute.dsde.workbench.avram.util.AvramResult
 
 trait SamDao {
-  @deprecated("Use getUserStatus instead", "11/1/2018")
-  def getUserStatus_deprecated(token: String): Either[AvramException, SamUserInfoResponse]
   def getUserStatus(token: String): AvramResult[SamUserInfoResponse]
   def queryAction(samResource: String, action: String, token: String): AvramResult[Boolean]
 }
